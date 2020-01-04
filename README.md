b7d365faf4275745d63bb42c354fe4e6d1e50e16
mauriziocarioli <maurizio.carioli@gmail.com>
2020-01-04 14:39:43 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Personal Health Management API with Node.js
+
+Read this blog for more information:
+
+https://maucarba.blogspot.com/2020/01/designing-event-driven-business-process.html
+
+## Getting Started
+
+### Prerequisites
+
+* Download [Node](https://nodejs.org/en/) and npm
+* `npm install` to install all node dependencies
+
+## Running the server
+
+* run `npm start` to start the server.
+
+## Built With
+
+* [Express](https://expressjs.com/) - Fast, unopinionated, minimalist web framework for Node.js
+* [Node.js](https://nodejs.org/en/) - Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.]
---
+++ app.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const port = process.env.PORT || 3200
+
+app.use(bodyparser.json())
+app.use(
+  bodyparser.urlencoded({
+    extended: false
+  })
+)
+
+app.get('/get_trigger/:trigger_id/:member_id', (req, res) => {
+  res.status(200).send([
+    /**
+     *  Getting Provider Info
+     */
+    {
+      task: {
+        id: 35,
+        origId: 'A490.0',
+        suppressed: false,
+        suppressionPeriod: '',
+        expirationDate: '2020-12-31T12:00:00.000Z',
+        close: 'HARD',
+        reminderInitiation: 'P15D',
+        reminderFrequency: 'R/P15D',
+        escalationTimer: 'P30D'
+      },
+      assignment: {
+        actor: 'Peter',
+        channel: 'MCC',
+        escalationActor: 'Patricia',
+        escalationChannel: 'CCN'
+      },
+      reminder: {
+        address: 'peter@doctor.com',
+        body: 'XYZ',
+        from: 'PHM@health_insurance.com',
+        subject: 'Reminder'
+      }
+    },
+    /**
+     *  Getting Community Health Worker Info
+     */
+    {
+      task: {
+        id: 58,
+        origId: 'B143',
+        suppressed: false,
+        suppressionPeriod: '',
+        expirationDate: '2020-12-31T12:00:00.000Z',
+        close: 'HARD',
+        reminderInitiation: 'P30D',
+        reminderFrequency: 'R/P30D',
+        //        reminderInitiation : 'PT60S',
+        //        reminderFrequency : 'R/PT60S',
+        escalationTimer: 'P90D'
+        //        escalationTimer : 'PT60S'
+      },
+      assignment: {
+        actor: 'Charlie',
+        channel: 'CCN',
+        escalationActor: 'Marc',
+        escalationChannel: 'CCN'
+      },
+      reminder: {
+        address: 'charlie@health_insurance.com',
+        body: 'XYZ',
+        from: 'PHM@health_insurance.com',
+        subject: 'Reminder'
+      }
+    },
+    /**
+     *  Getting Member Info
+     */
+    {
+      task: {
+        id: 128,
+        origId: 'C201',
+        predecessor: 'A490.0',
+        suppressed: false,
+        suppressionPeriod: '',
+        expirationDate: '2020-12-31T12:00:00.000Z',
+        close: 'SOFT',
+        reminderInitiation: 'P7D',
+        reminderFrequency: 'R/P7D',
+        escalationTimer: 'P30D'
+      },
+      assignment: {
+        actor: 'Mary',
+        channel: 'MLP',
+        escalationActor: 'Charlie',
+        escalationChannel: 'CCN'
+      },
+      reminder: {
+        address: 'mary@mail.com',
+        body: 'XYZ',
+        from: 'PHM@health_insurance.com',
+        subject: 'Reminder'
+      }
+    },
+    /**
+     *  Getting Pharmacist Info
+     */
+    {
+      task: {
+        id: 112,
+        origId: 'C178',
+        predecessor: 'A490.0',
+        suppressed: false,
+        suppressionPeriod: '',
+        expirationDate: '2020-12-31T12:00:00.000Z',
+        close: 'SOFT',
+        reminderInitiation: 'P7D',
+        reminderFrequency: 'R/P7D',
+        escalationTimer: 'P30D'
+      },
+      assignment: {
+        actor: 'Robert',
+        channel: 'CCN',
+        escalationActor: 'Matthew',
+        escalationChannel: 'CCN'
+      },
+      reminder: {
+        address: 'robert@pharmacy.com',
+        body: 'XYZ',
+        from: 'PHM@health_insurance.com',
+        subject: 'Reminder'
+      }
+    }
+  ])
+})
+
+app.listen(port, () => {
+  console.log(`running at port ${port}`)
+})
